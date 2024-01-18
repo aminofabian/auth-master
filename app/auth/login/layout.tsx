29d6eec2555c4fd1083892a394f06a4271b433b1
fabianamino <aminofabian@gmail.com>
2024-01-18 17:43:42 +0300

    create a database table with neon tech

--- app/auth/login/layout.tsx
+++ app/auth/login/layout.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function AuthLayout({
+export default async function AuthLayout({
   children
 }: {
   children: React.ReactNode
-}) {
+  }) {
   return (
     <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-500 to-slate-200">
     {children}
