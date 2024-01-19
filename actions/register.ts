78047c05b603b9a5e282de69a9dcda9775a6d158
fabianamino <aminofabian@gmail.com>
2024-01-19 18:53:48 +0300

    encrypted the password field

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,8 @@
 "use server"
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import db from "@/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,6 +12,30 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     };
 
   }
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10); 
+  const existingUser = await db.user.findUnique({
+    where: {
+        email
+      }
+  });
+  if (existingUser) {
+    return {
+      error: "Email already in use"
+    }
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword
+    
+    },
+  });
+    
+  //TODO: Send Verification Token Email
+    
   return {
     success: "Registration Successful"
   };
