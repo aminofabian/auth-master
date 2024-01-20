45856ba24a071d4a2332d08411e58a097f6443cc
fabianamino <aminofabian@gmail.com>
2024-01-20 13:30:59 +0300

    added matched to the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
 import { auth } from "./auth"
 
 export default auth((req) => {
-  // req.auth
+  const isLoggedIn = !!req.auth;
+  console.log("ROUTE: ", req.nextUrl.pathname);
+  console.log("isLoggedIn: ", isLoggedIn);
 })
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 }
