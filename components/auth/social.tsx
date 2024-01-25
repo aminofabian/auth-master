4fad3790ef1426c2b6c6835de472350ac61779cd
fabianamino <aminofabian@gmail.com>
2024-01-25 14:16:58 +0300

    configured Google and Github providers for logging

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -3,21 +3,28 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "../ui/button";
+import { signIn } from 'next-auth/react';
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
+  const onClick = (provider: 'google' | 'github') => {
+    signIn(provider), {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    };
+  };
   return (
     <div className="flex items-center w-full gap-3">
       <Button size="lg"
         className="w-full"
         variant="outline"
-        onClick={()=>{}}
+        onClick={()=>onClick('google')}
       >
     <FcGoogle className="h-5 w-5"/>
       </Button>
       <Button size="lg"
         className="w-full"
         variant="outline"
-        onClick={()=>{}}
+        onClick={()=>onClick('github')}
       >
     <FaGithub className="h-5 w-5"/>
     </Button>
