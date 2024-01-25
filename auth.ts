9d8736f7507ef02a29c84c3a438dfc16693cf9e0
fabianamino <aminofabian@gmail.com>
2024-01-25 14:51:58 +0300

    added an email verified event

--- auth.ts
+++ auth.ts
@@ -6,12 +6,26 @@ import db from "@/lib/db";
 
 
 
+
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
 } = NextAuth({
+  pages: {
+    
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: {
+          emailVerified: new Date()
+        }
+      });
+    }
+  },
   callbacks: {
     async session({ token, session }) {
       if (token.sub && session.user) {
