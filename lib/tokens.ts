05d428b7c42364558b49147eebd12db288f061c9
fabianamino <aminofabian@gmail.com>
2024-01-27 15:25:56 +0300

    used uuid to generate verification code

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,5 +1,26 @@
+import { getVerificationTokenByEmail } from '@/data/verification-token';
+import { v4 as uuidv4 } from 'uuid';
+import db from '@/lib/db';
 
+export const generateVerificationToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
 
-export const generateVerificationToken = (email: string) => {
-  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-};  
+  const existingToken = await getVerificationTokenByEmail(email);
+
+  if (existingToken) {
+    await db.verificationToken.delete({
+      where: {
+        id: existingToken?.id,
+      },
+    });
+    const verificationToken = await db.verificationToken.create({
+      data: {
+        token,
+        expires,
+        email,
+      },
+    });
+    return verificationToken;
+  }
+};
