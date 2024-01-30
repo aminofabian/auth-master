2a4486139ac3be0516ad444f6268bedf64c88d05
fabianamino <aminofabian@gmail.com>
2024-01-30 17:39:39 +0300

    added the verifiation link to the login page as well

--- actions/login.ts
+++ actions/login.ts
@@ -6,6 +6,8 @@ import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
 import { generateVerificationToken } from "@/lib/tokens";
 import { getUserByEmail } from "@/data/user";
+import { sendVerificationEmail } from "@/lib/mail";
+
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -24,7 +26,8 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
   }
 
   if (!existingUser.emailVerified) {
-    const verificationToken = await generateVerificationToken(email);
+    const verificationToken = await generateVerificationToken(existingUser.email);
+    await sendVerificationEmail(verificationToken.email, verificationToken.token);
     return {
       success: "Check Your email for a verification link to continue"
     }
